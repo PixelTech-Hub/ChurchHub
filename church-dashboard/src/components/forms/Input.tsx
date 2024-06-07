ea4e8d40460f1547d2c59423f0b2d88625c83a69
Okumu Daniel Comboni <okumucomboni@gmail.com>
2024-06-07 17:20:42 +0300

    dashboard: login ui done

--- church-dashboard/src/components/forms/Input.tsx
+++ church-dashboard/src/components/forms/Input.tsx
@@ -11,10 +11,10 @@ interface Props {
 const Input = ({ value, label, type, placeholder, setValue }: Props) => {
 	return (
 		<div className=' gap-2'>
-			<label htmlFor="name" className='text-gray-500 lg:text-[16px] text:[12px]'>{label}:</label>
+			<label htmlFor="name" className='font-semibold lg:text-lg text:[12px]'>{label}:</label>
 			<input
 				type={type}
-				className='outline-[#200e32] bg-gray-200 text-black w-full h-[2.5rem] px-2 rounded-md p-1 lg:text-[16px] text-[12px]'
+				className='outline-[#49d205] bg-gray-200 text-black w-full h-[2.5rem] px-2 rounded-md p-1 lg:text-[16px] text-[12px]'
 				placeholder={placeholder}
 				value={value}
 				onChange={(e) => setValue(e.target.value)}
--- church-dashboard/src/pages/login/index.tsx
+++ church-dashboard/src/pages/login/index.tsx
@@ -1,5 +1,6 @@
 import Input from "@/components/forms/Input"
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
 
 const Login = () => {
@@ -8,27 +9,30 @@ const Login = () => {
 
 	return (
 		<div className="relative bg-image">
-			<div className="absolute lg:top-[20rem] md:top-[25rem] top-[10rem] lg:left-[40rem] md:left-[8rem] left-[1rem] lg:right-[40rem] md:right-[8rem] right-[1rem]  bg-white rounded-lg p-4 pt-10 pb-10 ">
-				<div className="font-bold lg:text-xl text-lg flex items-center justify-center">
-					<h1>CHURCHPRO LOGIN </h1>
+			<div className="border border-[#49d205] absolute lg:top-[20rem] h-[27rem] md:top-[25rem] top-[10rem] lg:left-[40rem] md:left-[8rem] left-[1rem] lg:right-[40rem] md:right-[8rem] right-[1rem]  bg-white rounded-lg  pt-10 pb-10 ">
+				<div className="font-extrabold flex flex-col items-center justify-center">
+					<h1 className="lg:text-3xl text-xl"> CHURCHPRO </h1>
+					<h1 className="lg:text-xl text-sm"> ADMIN LOGIN </h1>
+
 				</div>
-				<form className="flex flex-col space-y-3 m-2">
+				<div className="h-[2px] w-auto px-0 bg-gray-500 mt-4" />
+				<form className="flex flex-col space-y-3 m-2 mt-8 p-2 ">
 					<div>
-						<Input value={email} setValue={setEmail} label="Email" type="text" placeholder="admin@church.org" />
+						<Input value={email} setValue={setEmail} label="Email" type="text" placeholder="" />
 					</div>
 					<div>
-						<Input value={password} setValue={setPassword} label="Password" type="text" placeholder="admin@church.org" />
+						<Input value={password} setValue={setPassword} label="Password" type="password" placeholder="" />
 					</div>
-					<div className="flex flex-row items-center justify-between ">
-						<p className="cursor-pointer text-red-500 lg:text-[16px] text-[12px]">Forgot password?</p>
-						<p className="cursor-pointer lg:text-[16px] text-[12px]">Terms of Condition</p>
+					<div className="flex flex-row items-center justify-between mb-4">
+						<Link to='/forgot-password' className="cursor-pointer text-red-500 lg:text-[16px] text-[12px]">Forgot password?</Link>
+						<Link to='/terms-and-conditions' className="cursor-pointer  text-[#49d205] lg:text-[16px] text-[12px]">Terms of Condition</Link>
 					</div>
-					<div className="bg-green-500 p-2 flex items-center justify-center rounded-lg ">
+					<div className=" bg-[#49d205] p-2 flex items-center justify-center rounded-lg ">
 						<button className="uppercase font-semibold lg:text-base text-sm">login</button>
 					</div>
 				</form>
 			</div>
-			<div className="absolute lg:bottom-4 bottom-2 left-0 right-0 flex items-center justify-center  lg:px-4 px-2 font-bold lg:text-xl text-lg lg:text-black text-white ">
+			<div className="absolute lg:bottom-4 bottom-2 left-0 right-0 flex items-center justify-center  lg:px-4 px-2 font-bold lg:text-xl md:text-lg text-sm text-white ">
 				<p className="">Designed by PixelTech 2024</p>
 			</div>
 		</div>
