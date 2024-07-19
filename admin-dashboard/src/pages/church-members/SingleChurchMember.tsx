d924173b10affe004e3f0dc5c9481a96a9d432f4
Okumu Daniel Comboni <okumucomboni@gmail.com>
2024-07-19 10:26:08 +0300

    admin:dashboard update ministries per member

--- admin-dashboard/src/pages/church-members/SingleChurchMember.tsx
+++ admin-dashboard/src/pages/church-members/SingleChurchMember.tsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { API_BASE_URL } from "../../app/api";
 import NavbarSidebarLayout from "../../layouts/navbar-sidebar";
-import { Breadcrumb, Progress } from "flowbite-react";
-import { HiAcademicCap, HiHeart, HiHome, HiMail, HiOfficeBuilding, HiPhone, HiUsers, HiClock, HiUser } from "react-icons/hi";
+import { Breadcrumb, Button, Progress } from "flowbite-react";
+import { HiAcademicCap, HiHeart, HiHome, HiMail, HiOfficeBuilding, HiPhone, HiUsers, HiClock, HiUser, HiPlus } from "react-icons/hi";
 import { EntityAgeEnum } from "../../enums/entity-age.enum";
 import { EntityGenderEnum } from "../../enums/entity-gender.enum";
 import { EntityMaritalStatusEnum } from "../../enums/entity-maritalstatus.enum";
@@ -12,6 +12,8 @@ import { EntityEducationalLevelEnum } from "../../enums/entity-education.enum";
 import { toast } from "react-toastify";
 import ChurchMemberInfoItem from "../../components/church-members/ChurchMemberInfoItem";
 import { ChurchMembers } from "../../types/ChurchMember";
+import { ChurchMinistries } from "../../types/ChurchMinistries";
+import AddMinistryModal from "../../components/church-members/AddMinistryModal";
 
 
 
@@ -21,6 +23,8 @@ const SingleChurchMember = () => {
 	const { id } = useParams<{ id: string }>();
 	const [member, setMember] = useState<ChurchMembers | null>(null);
 	const [isLoading, setIsLoading] = useState(true);
+
+
 
 	useEffect(() => {
 		fetchMemberDetails();
@@ -67,62 +71,89 @@ const SingleChurchMember = () => {
 	// };
 	const updateMemberField = async (field: keyof ChurchMembers, value: string) => {
 		try {
-		  let validatedValue: string | EntityAgeEnum | EntityGenderEnum | EntityMaritalStatusEnum | EntityEducationalLevelEnum = value;
-	  
-		  switch (field) {
-			case 'gender':
-			  if (!Object.values(EntityGenderEnum).includes(value as EntityGenderEnum)) {
-				throw new Error('Invalid gender value');
-			  }
-			  validatedValue = value as EntityGenderEnum;
-			  break;
-			case 'age':
-			  if (!Object.values(EntityAgeEnum).includes(value as EntityAgeEnum)) {
-				throw new Error('Invalid age range');
-			  }
-			  validatedValue = value as EntityAgeEnum;
-			  break;
-			case 'marital_status':
-			  if (!Object.values(EntityMaritalStatusEnum).includes(value as EntityMaritalStatusEnum)) {
-				throw new Error('Invalid marital status');
-			  }
-			  validatedValue = value as EntityMaritalStatusEnum;
-			  break;
-			case 'education_level':
-			  if (!Object.values(EntityEducationalLevelEnum).includes(value as EntityEducationalLevelEnum)) {
-				throw new Error('Invalid education level');
-			  }
-			  validatedValue = value as EntityEducationalLevelEnum;
-			  break;
-			case 'email':
-			  // Simple email validation
-			  if (!/^\S+@\S+\.\S+$/.test(value)) {
-				throw new Error('Invalid email format');
-			  }
-			  break;
-			case 'phone_number':
-			  // Simple phone number validation (you might want to use a library for more robust validation)
-			  if (!/^\+?[0-9]{10,14}$/.test(value)) {
-				throw new Error('Invalid phone number format');
-			  }
-			  break;
-			// Add other field-specific validations as needed
-		  }
-	  
-		  const response = await axios.patch(`${API_BASE_URL}/church-members/${id}`, {
-			[field]: validatedValue
-		  });
-		  
-		  if (response.status === 200) {
-			setMember(prevMember => prevMember ? { ...prevMember, [field]: validatedValue } : null);
-			toast.success(`${field.replace('_', ' ')} updated successfully!`);
-		  }
+			let validatedValue: string | EntityAgeEnum | EntityGenderEnum | EntityMaritalStatusEnum | EntityEducationalLevelEnum = value;
+
+			switch (field) {
+				case 'gender':
+					if (!Object.values(EntityGenderEnum).includes(value as EntityGenderEnum)) {
+						throw new Error('Invalid gender value');
+					}
+					validatedValue = value as EntityGenderEnum;
+					break;
+				case 'age':
+					if (!Object.values(EntityAgeEnum).includes(value as EntityAgeEnum)) {
+						throw new Error('Invalid age range');
+					}
+					validatedValue = value as EntityAgeEnum;
+					break;
+				case 'marital_status':
+					if (!Object.values(EntityMaritalStatusEnum).includes(value as EntityMaritalStatusEnum)) {
+						throw new Error('Invalid marital status');
+					}
+					validatedValue = value as EntityMaritalStatusEnum;
+					break;
+				case 'education_level':
+					if (!Object.values(EntityEducationalLevelEnum).includes(value as EntityEducationalLevelEnum)) {
+						throw new Error('Invalid education level');
+					}
+					validatedValue = value as EntityEducationalLevelEnum;
+					break;
+				case 'email':
+					// Simple email validation
+					if (!/^\S+@\S+\.\S+$/.test(value)) {
+						throw new Error('Invalid email format');
+					}
+					break;
+				case 'phone_number':
+					// Simple phone number validation (you might want to use a library for more robust validation)
+					if (!/^\+?[0-9]{10,14}$/.test(value)) {
+						throw new Error('Invalid phone number format');
+					}
+					break;
+				// Add other field-specific validations as needed
+			}
+
+			const response = await axios.patch(`${API_BASE_URL}/church-members/${id}`, {
+				[field]: validatedValue
+			});
+
+			if (response.status === 200) {
+				setMember(prevMember => prevMember ? { ...prevMember, [field]: validatedValue } : null);
+				toast.success(`${field.replace('_', ' ')} updated successfully!`);
+			}
 		} catch (error) {
-		  console.error('Error updating member field:', error);
-		  toast.error(`Failed to update ${field.replace('_', ' ')}. Please try again.`);
-		  // You might want to show an error message to the user here
+			console.error('Error updating member field:', error);
+			toast.error(`Failed to update ${field.replace('_', ' ')}. Please try again.`);
+			// You might want to show an error message to the user here
 		}
-	  };
+	};
+
+
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const handleAddMinistries = async (selectedMinistries: ChurchMinistries[]) => {
+		try {
+			const response = await axios.patch(`${API_BASE_URL}/church-members/${id}`, {
+				church_ministries_ids: selectedMinistries.map(m => m.id)
+			});
+			if (response.status === 200) {
+				setMember(prevMember => {
+					if (!prevMember) return null;
+					return {
+						...prevMember,
+						church_ministries_ids: [
+							...(prevMember.church_ministries_ids || []),
+							...selectedMinistries.filter(m => !prevMember.church_ministries_ids?.some(existingM => existingM.id === m.id))
+						]
+					};
+				});
+				toast.success('Ministries updated successfully!');
+			}
+		} catch (error) {
+			console.error('Error updating ministries:', error);
+			toast.error('Failed to update ministries. Please try again.');
+		}
+	};
 
 	return (
 		<NavbarSidebarLayout>
@@ -145,7 +176,7 @@ const SingleChurchMember = () => {
 				) : member && (
 					<div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden">
 						<div className="bg-gradient-to-r from-blue-500 to-purple-600 p-8 text-white relative">
-							
+
 							<h1 className="lg:text-4xl md:text-3xl text-2xl font-bold mb-2">{member.full_name}</h1>
 							<p className="lg:text-lg text-sm opacity-80">{member.gender}</p>
 						</div>
@@ -162,7 +193,7 @@ const SingleChurchMember = () => {
 
 							<div className="bg-gradient-to-r from-blue-500 to-purple-600 dark:bg-gray-700 p-6 rounded-lg">
 								<h2 className="lg:text-2xl text-xl dark:text-white font-semibold mb-4 flex items-center">
-									<HiUsers className="mr-2" color="white"/> Age Group
+									<HiUsers className="mr-2" color="white" /> Age Group
 								</h2>
 								<div className="text-center mb-4">
 									<span className="dark:text-white lg:text-4xl md:text-3xl text-2xl font-bold">{member.age}</span>
@@ -177,25 +208,33 @@ const SingleChurchMember = () => {
 
 						<div className="bg-gray-50 dark:bg-gray-900 lg:p-8 p-0 pt-8">
 							<h2 className="lg:text-2xl text-xl dark:text-white lg:text-start text-center font-semibold mb-6">Ministries</h2>
+							<Button onClick={() => setIsModalOpen(true)} className="bg-blue-600 hover:bg-blue-700 text-white">
+								<HiPlus className="mr-2" /> Add Ministries
+							</Button>
 							{member.church_ministries_ids?.length > 0 ? (
-								<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-								{member.church_ministries_ids.map((ministry) => (
-									<div key={ministry.id} className="bg-white dark:text-white dark:bg-gray-800 p-6 rounded-lg shadow-md transform hover:scale-105 transition-transform duration-200">
-										<h3 className="font-bold text-xl mb-2 text-blue-600 dark:text-blue-400">{ministry.name}</h3>
-										<p className="text-sm opacity-70 mb-3 flex items-center">
-											<HiUser className="mr-2" /> Leader: {ministry.leader}
-										</p>
-										<p className="text-sm">{ministry.description}</p>
-									</div>
-								))}
-							</div>
-							): (
+								<div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+									{member.church_ministries_ids.map((ministry) => (
+										<div key={ministry.id} className="bg-white dark:text-white dark:bg-gray-800 p-6 rounded-lg shadow-md transform hover:scale-105 transition-transform duration-200">
+											<h3 className="font-bold text-xl mb-2 text-blue-600 dark:text-blue-400">{ministry.name}</h3>
+											<p className="text-sm opacity-70 mb-3 flex items-center">
+												<HiUser className="mr-2" /> Leader: {ministry.leader}
+											</p>
+											<p className="text-sm">{ministry.description}</p>
+										</div>
+									))}
+								</div>
+							) : (
 								<>
-								<p className="text-yellow-600 text-center pb-6 font-semibold">You are not involed in any ministry</p>
+									<p className="text-yellow-600 text-center pb-6 font-semibold">You are not involed in any ministry</p>
 								</>
 							)}
 						</div>
-
+						<AddMinistryModal
+							isOpen={isModalOpen}
+							onClose={() => setIsModalOpen(false)}
+							onAddMinistries={handleAddMinistries}
+							currentMinistries={member?.church_ministries_ids || []}
+						/>
 						<div className="bg-gray-200 dark:bg-gray-700 p-4 text-center">
 							<p className="text-sm flex items-center justify-center">
 								<HiClock className="mr-2" /> Last updated: {new Date(member.updatedAt).toLocaleDateString()}
