42ec5838f99ab929a1cb1d2f349b496483a6dded
Okumu Daniel Comboni <okumucomboni@gmail.com>
2024-08-02 01:07:49 +0300

    church member slice completed

--- admin-dashboard/src/features/church-members/memberSlice.ts
+++ admin-dashboard/src/features/church-members/memberSlice.ts
@@ -1,159 +1,161 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { ChurchMinistries } from "../../types/ChurchMinistries";
-import ChurchMinistriesService from "./memberService";
+import { ChurchMembers } from "../../types/ChurchMember";
+import ChurchMemberService from "./memberService";
 
-export interface ChurchMinistryState {
-    allChurchMinistry: ChurchMinistries[] | null;
-    churchMinistry: ChurchMinistries | null;
+export interface ChurchMemberState {
+    allChurchMembers: ChurchMembers[] | null;
+    churchMember: ChurchMembers | null;
     loading: boolean;
     error: string | null;
     posting: boolean;
     postError: string | null;
-    deleting: boolean; // New state for tracking delete status
+    deleting: boolean;
+    updating: boolean // New state for tracking delete status
     deleteError: string | null; // New state for tracking delete errors
 }
 
-const initialState: ChurchMinistryState = {
-    allChurchMinistry: [],
-    churchMinistry: null,
+const initialState: ChurchMemberState = {
+    allChurchMembers: [],
+    churchMember: null,
     loading: false,
     error: null,
     posting: false,
     postError: null,
     deleting: false,
     deleteError: null,
+    updating: false
 };
 
-// Thunk for getting all church ministries
-export const getAllChurchMinistries = createAsyncThunk("churches_ministries/get-all", 
+// Thunk for getting all church members
+export const getAllChurchMembers = createAsyncThunk("church_members/get-all", 
     async (churchId: string, { rejectWithValue }) => {
         try {
-            return await ChurchMinistriesService.getAllChurchMinistries(churchId);
+            return await ChurchMemberService.getAllChurchMembers(churchId);
         } catch (error) {
             return rejectWithValue((error as Error).message);
         }
     }
 );
 
-// Thunk for posting a new church ministry
-export const postNewChurchMinistry = createAsyncThunk("churches_ministries/post", 
-    async (ministryData: ChurchMinistries, { rejectWithValue }) => {
+// Thunk for posting a new church member
+export const postNewChurchMember = createAsyncThunk("church_members/post", 
+    async (memberData: ChurchMembers, { rejectWithValue }) => {
         try {
-            return await ChurchMinistriesService.postNewChurchMinistry(ministryData);
+            return await ChurchMemberService.postNewChurchMember(memberData);
         } catch (error) {
             return rejectWithValue((error as Error).message);
         }
     }
 );
 
-// Thunk for getting a single church ministry
-export const getChurchMinistryById = createAsyncThunk("churches_ministries/get-by-id", 
+// Thunk for getting a single church members
+export const getChurchMemberById = createAsyncThunk("church_members/get-by-id", 
     async (ministryId: string, { rejectWithValue }) => {
         try {
-            return await ChurchMinistriesService.getChurchMinistryById(ministryId);
+            return await ChurchMemberService.getChurchMemberById(ministryId);
         } catch (error) {
             return rejectWithValue((error as Error).message);
         }
     }
 );
 
-// Thunk for updating a church ministry
-export const updateChurchMinistry = createAsyncThunk("churches_ministries/update", 
-    async ({ ministryId, ministryData }: { ministryId: string, ministryData: Partial<ChurchMinistries> }, { rejectWithValue }) => {
+// Thunk for updating a church member
+export const updateChurchMember = createAsyncThunk("church_members/update", 
+    async ({ memberId, memberData }: { memberId: string, memberData: Partial<ChurchMembers> }, { rejectWithValue }) => {
         try {
-            return await ChurchMinistriesService.updateChurchMinistry(ministryId, ministryData);
+            return await ChurchMemberService.updateChurchMember(memberId, memberData);
         } catch (error) {
             return rejectWithValue((error as Error).message);
         }
     }
 );
 
-// Thunk for deleting a church ministry
-export const deleteChurchMinistry = createAsyncThunk("churches_ministries/delete", 
-    async (ministryId: string, { rejectWithValue }) => {
+// Thunk for deleting a church member
+export const deleteChurchMember = createAsyncThunk("churches_ministries/delete", 
+    async (memberId: string, { rejectWithValue }) => {
         try {
-            await ChurchMinistriesService.deleteChurchMinistry(ministryId);
-            return ministryId;
+            await ChurchMemberService.deleteChurchMember(memberId);
+            return memberId;
         } catch (error) {
             return rejectWithValue((error as Error).message);
         }
     }
 );
 
-export const churchMinistrySlice = createSlice({
-    name: "church_ministries",
+export const churchMemberSlice = createSlice({
+    name: "church_members",
     initialState,
     reducers: {},
     extraReducers: (builder) => {
         builder
-            .addCase(getAllChurchMinistries.pending, (state) => {
+            .addCase(getAllChurchMembers.pending, (state) => {
                 state.loading = true;
                 state.error = null;
             })
-            .addCase(getAllChurchMinistries.fulfilled, (state, action) => {
+            .addCase(getAllChurchMembers.fulfilled, (state, action) => {
                 state.loading = false;
-                state.allChurchMinistry = action.payload;
+                state.allChurchMembers = action.payload;
             })
-            .addCase(getAllChurchMinistries.rejected, (state, action) => {
+            .addCase(getAllChurchMembers.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.error.message || "Failed to fetch church services";
             })
-            .addCase(postNewChurchMinistry.pending, (state) => {
+            .addCase(postNewChurchMember.pending, (state) => {
                 state.posting = true;
                 state.postError = null;
             })
-            .addCase(postNewChurchMinistry.fulfilled, (state, action) => {
+            .addCase(postNewChurchMember.fulfilled, (state, action) => {
                 state.posting = false;
-                state.allChurchMinistry = state.allChurchMinistry ? [...state.allChurchMinistry, action.payload] : [action.payload];
+                state.allChurchMembers = state.allChurchMembers ? [...state.allChurchMembers, action.payload] : [action.payload];
             })
-            .addCase(postNewChurchMinistry.rejected, (state, action) => {
+            .addCase(postNewChurchMember.rejected, (state, action) => {
                 state.posting = false;
                 state.postError = action.error.message || "Failed to post new church branch";
             })
-            .addCase(getChurchMinistryById.pending, (state) => {
+            .addCase(getChurchMemberById.pending, (state) => {
                 state.loading = true;
                 state.error = null;
             })
-            .addCase(getChurchMinistryById.fulfilled, (state, action) => {
+            .addCase(getChurchMemberById.fulfilled, (state, action) => {
                 state.loading = false;
-                state.churchMinistry = action.payload;
+                state.churchMember = action.payload;
             })
-            .addCase(getChurchMinistryById.rejected, (state, action) => {
+            .addCase(getChurchMemberById.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.error.message || "Failed to fetch church branch";
             })
-            .addCase(updateChurchMinistry.pending, (state) => {
+            .addCase(updateChurchMember.pending, (state) => {
                 state.loading = true;
                 state.error = null;
             })
-            .addCase(updateChurchMinistry.fulfilled, (state, action) => {
+            .addCase(updateChurchMember.fulfilled, (state, action) => {
                 state.loading = false;
-                if (state.allChurchMinistry) {
-                    const index = state.allChurchMinistry.findIndex(ministry => ministry.id === action.payload.id);
+                if (state.allChurchMembers) {
+                    const index = state.allChurchMembers.findIndex(member => member.id === action.payload.id);
                     if (index !== -1) {
-                        state.allChurchMinistry[index] = action.payload;
+                        state.allChurchMembers[index] = action.payload;
                     }
                 }
             })
-            .addCase(updateChurchMinistry.rejected, (state, action) => {
+            .addCase(updateChurchMember.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.error.message || "Failed to update church branch";
             })
-            .addCase(deleteChurchMinistry.pending, (state) => {
+            .addCase(deleteChurchMember.pending, (state) => {
                 state.deleting = true;
                 state.deleteError = null;
             })
-            .addCase(deleteChurchMinistry.fulfilled, (state, action) => {
+            .addCase(deleteChurchMember.fulfilled, (state, action) => {
                 state.deleting = false;
-                if (state.allChurchMinistry) {
-                    state.allChurchMinistry = state.allChurchMinistry.filter(ministry => ministry.id !== action.payload);
+                if (state.allChurchMembers) {
+                    state.allChurchMembers = state.allChurchMembers.filter(member => member.id !== action.payload);
                 }
             })
-            .addCase(deleteChurchMinistry.rejected, (state, action) => {
+            .addCase(deleteChurchMember.rejected, (state, action) => {
                 state.deleting = false;
                 state.deleteError = action.error.message || "Failed to delete church branch";
             });
     },
 });
 
-export default churchMinistrySlice.reducer;
+export default churchMemberSlice.reducer;
